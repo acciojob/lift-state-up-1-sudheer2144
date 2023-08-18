52b0dc33a744bec5469d02f604682517c6e21590
sudheer2144 <sudheer.ged@gmail.com>
2023-08-18 22:28:07 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [show, setShow] = useState(false);
+
+  function changeShow() {
+    setShow(!show);
+  }
+
+  const Model = () => {
+    return (
+      <div>
+        <h2>Model Content</h2>
+        <p>This is the model content</p>
+      </div>
+    )
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <div>
+        <h1>Child Component</h1>
+        <button onClick={changeShow}>Show Model</button>
+        {show ? <Model /> : null}
+      </div>
     </div>
   )
+
 }
 
 export default App
