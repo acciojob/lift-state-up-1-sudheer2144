3fefd80b416fee4505d9c57d0929fe37fe51c0d9
sudheer2144 <sudheer.ged@gmail.com>
2023-08-18 22:38:58 +0530

    submitting code-Fri Aug 18 2023 22:38:56 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ const App = () => {
     return (
       <div>
         <h3>Modal Content</h3>
-        <p>This is the modal content</p>
+        <p>This is the modal content.</p>
       </div>
     )
   }
